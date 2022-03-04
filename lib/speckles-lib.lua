cf6869fb752931ba278746925b0fa13a37cddc10
bgc <bgc@users.noreply.github.com>
2022-03-04 19:38:49 +0700

    Updating UI functionality

--- lib/speckles-lib.lua
+++ lib/speckles-lib.lua
@@ -1,76 +1,73 @@
 ---
 -- helpers for Speckles
 ---
-
 function setupControls()
-  params:add_separator("Oscillator")
-  -- Param / Controlspec for amplitude
-  params:add{
-    type="control",
-    id="amp",
-    controlspec=controlspec.new(
-      0, --minVal
-      1, --maxVal
-      "linear", --warp (exp, db, lin)
-      0, -- step (rounded)
-      1, --default initial value
-      "" --units
-    ),
-    action=engine.amp,
-  }
+    params:add_separator("Oscillator")
+    -- Param / Controlspec for amplitude
+    params:add{
+        type = "control",
+        id = "amp",
+        controlspec = controlspec.new(0, -- minVal
+        1, -- maxVal
+        "linear", -- warp (exp, db, lin)
+        0, -- step (rounded)
+        1, -- default initial value
+        "" -- units
+        ),
+        action = engine.amp
+    }
 
-  -- Param / Controlspec for engine density
-  params:add{
-    type="control",
-    id="density",
-    controlspec=controlspec.new(
-      0.01, --minVal
-      440, --maxVal
-      "exp", --warp (exp, db, lin)
-      0, -- step (rounded)
-      1, --default initial value
-      "Hz" --units
-    ),
-    action=engine.density,
-  }
+    -- Param / Controlspec for engine density
+    params:add{
+        type = "control",
+        id = "density",
+        controlspec = controlspec.new(0.01, -- minVal
+        440, -- maxVal
+        "exp", -- warp (exp, db, lin)
+        0, -- step (rounded)
+        1, -- default initial value
+        "Hz" -- units
+        ),
+        action = engine.density
+    }
 
-  params:add_separator("Filter")
-  -- filter type lp/hp
-  params:add_number("filter", "filter", 0, 3, 0)
-  params:set_action("filter", function(v) engine.filter(v) end)
-  
-  params:add{
-    type="control",
-    id="filter_freq",
-    controlspec=controlspec.new(
-      0.01, --minVal
-      20000, --maxVal
-      "exp", --warp (exp, db, lin)
-      0, -- step (rounded)
-      440, --default initial value
-      "Hz" --units
-    ),
-    action=engine.filter_freq,
-  }
-  params:add{
-    type="control",
-    id="reso",
-    controlspec=controlspec.new(
-      0, --minVal
-      1, --maxVal
-      "lin", --warp (exp, db, lin)
-      0.01, -- step (rounded)
-      0, --default initial value
-      "" --units
-    ),
-    action=engine.reso,
-  }
-  -- params:add_control("filter_freq", "filter_freq", controlspec.WIDEFREQ)
-  -- params:set_action("filter_freq", function(v) engine.filter_freq(v) end)
+    params:add_separator("Filter")
+    -- filter type lp/hp
+    params:add_number("filter", "filter", 0, 3, 0)
+    params:set_action("filter", function(v)
+        engine.filter(v)
+    end)
 
-  -- params:add_control("reso", "reso", controlspec.RQ)
-  -- params:set_action("reso", function(v) engine.reso(v) end)
-  params:add_separator()
+    params:add{
+        type = "control",
+        id = "filter_freq",
+        controlspec = controlspec.new(0.01, -- minVal
+        20000, -- maxVal
+        "exp", -- warp (exp, db, lin)
+        0, -- step (rounded)
+        440, -- default initial value
+        "Hz" -- units
+        ),
+        action = engine.filter_freq
+    }
+    params:add{
+        type = "control",
+        id = "reso",
+        controlspec = controlspec.new(0, -- minVal
+        1, -- maxVal
+        "lin", -- warp (exp, db, lin)
+        0.01, -- step (rounded)
+        0, -- default initial value
+        "" -- units
+        ),
+        action = engine.reso
+    }
+    -- params:add_control("filter_freq", "filter_freq", controlspec.WIDEFREQ)
+    -- params:set_action("filter_freq", function(v) engine.filter_freq(v) end)
+
+    -- params:add_control("reso", "reso", controlspec.RQ)
+    -- params:set_action("reso", function(v) engine.reso(v) end)
+    params:add_separator()
 
 end
 
@@ -81,24 +78,50 @@ end
 -- function
 -- Button 2 - Change Page
 
-function drawInstructions ()
-  screen.move(1, 8)
-  screen.text("CONTROLS")
-  screen.stroke()
-  screen.close()
-  
-  screen.move(1, 18)
-  screen.text("E1 - Volume")
-  screen.stroke()
-  screen.close()
-  
-  screen.move(1, 28)
-  screen.text("B2 - Change Page")
-  screen.stroke()
-  screen.close()
-  
-  screen.move(1, 38)
-  screen.text("B3, E2, E3 Page dependant")
-  screen.stroke()
-  screen.close()
+function drawInstructions()
+    screen.move(1, 8)
+    screen.text("CONTROLS")
+    screen.stroke()
+    screen.close()
+
+    screen.move(1, 18)
+    screen.text("E1 - Volume")
+    screen.stroke()
+    screen.close()
+
+    screen.move(1, 28)
+    screen.text("B2 - Change Page")
+    screen.stroke()
+    screen.close()
+
+    screen.move(1, 38)
+    screen.text("B3, E2, E3 Page dependant")
+    screen.stroke()
+    screen.close()
+end
+
+-- Handles the changing of filter type with button 3 on page 2
+function handleFilterTypeControl()
+    local value = params:get("filter")
+    if value == 3 then
+      -- so it wraps!
+        params:set("filter", 0)
+    else
+        params:set("filter", value + 1)
+    end
+    -- we get the new value in order to update UI
+    local updatedValue = params:get("filter")
+    -- the +1 is cause Lua uses 1 based index...
+    -- update the current filterType UI
+    theSpeckles.filterUI.filterTypes:set_index(updatedValue + 1)
+    if updatedValue ~= 0 then
+      -- set it active if not disabled
+      local filterTypeIndex = theSpeckles.filterUI.filterTypes.index
+      theSpeckles.filterUI.filter:set_active(true)
+      -- update the filter graph with new type
+      theSpeckles.filterUI.filter:edit(theSpeckles.filterUI.filters[filterTypeIndex])
+    else
+      -- disabled we do not want the filter graph active
+      theSpeckles.filterUI.filter:set_active(false)
+    end
 end
--- speckles.lua
+++ speckles.lua
@@ -42,6 +42,12 @@ engine.name = "Speckles"
 -- Imports
 local UI = require "ui"
 local ControlSpec = require "controlspec"
+local fGraph = require "filtergraph"
+
+-- Engine Global
+theSpeckles = {}
+theSpeckles.oscUI = {}
+theSpeckles.filterUI = {}
 
 -- libs
 local speckles_lib = include "lib/speckles-lib"
@@ -50,10 +56,7 @@ local speckles_lib = include "lib/speckles-lib"
 local SCREEN_FRAMERATE = 15
 local screen_refresh_metro
 
-local theSpeckles = {}
-theSpeckles.pages = UI.Pages.new(1, 2)
 
-local oscUI = {}
 
 local function setupScreen()
     screen.level(15)
@@ -64,8 +67,14 @@ local function setupScreen()
 end
 
 local function setupUI()
+    theSpeckles.pages = UI.Pages.new(1, 3)
     -- UI.Dial.new (x, y, size, value, min_value, max_value, rounding, start_value, markers, units, title)
-    oscUI.freq = UI.Dial.new(12, 34, 20, params:get('density'), 0.01, 440, 0.01, 1, {}, 'i/s')
+    theSpeckles.oscUI.freq = UI.Dial.new(12, 34, 20, params:get('density'), 0.01, 440, 0.01, 1, {}, 'i/s')
+    theSpeckles.filterUI.filters = {"disabled", "lowpass", "bandpass", "highpass"}
+    theSpeckles.filterUI.filter = fGraph.new()
+    theSpeckles.filterUI.filter:set_active(false)
+    theSpeckles.filterUI.filter:set_position_and_size(2, 22, 56, 36)
+    theSpeckles.filterUI.filterTypes = UI.List.new(74, 22, 1, theSpeckles.filterUI.filters)
 end
 
 -- initialization
@@ -98,12 +107,7 @@ function key(button, state)
 
     if screen == 2 then
         if button == 3 and state == 1 then
-            local value = params:get("filter")
-            if value == 3 then
-                params:set("filter", 0)
-            else
-                params:set("filter", value + 1)
-            end
+            handleFilterTypeControl()
         end
     end
     -- always have redraw at the end
@@ -124,11 +128,16 @@ function enc(encoder, delta)
         -- screen 1 encoder 2 is the engine density
         if screen == 1 then
             params:delta("density", delta)
-            oscUI.freq:set_value(params:get("density"))
+            theSpeckles.oscUI.freq:set_value(params:get("density"))
         end
         -- screen 2 encoder 2 is filter frequency
         if screen == 2 then
-            params:delta("filter_freq", delta)
+            local currentFilterType = theSpeckles.filterUI.filter:filter_type()
+            local filterSlope = theSpeckles.filterUI.filter:get_slope()
+            local resonance = params:get("reso")
+            params:delta("filter_freq", delta) 
+            local filterFreq = params:get("filter_freq")
+            theSpeckles.filterUI.filter:edit(currentFilterType, filterSlope, filterFreq, resonance)
         end
     end
 
@@ -136,7 +145,12 @@ function enc(encoder, delta)
     if encoder == 3 then
         -- screen 2 encoder 3 is filter resonance
         if screen == 2 then
+            local currentFilterType = theSpeckles.filterUI.filter:filter_type()
+            local filterSlope = theSpeckles.filterUI.filter:get_slope()
+            local filterFreq = theSpeckles.filterUI.filter:get_freq()
             params:delta("reso", delta)
+            local resonance = params:get("reso")
+            theSpeckles.filterUI.filter:edit(currentFilterType, filterSlope, filterFreq, resonance)
         end
     end
 
@@ -151,17 +165,33 @@ function redraw()
 
     -- redraw pages control
     theSpeckles.pages:redraw()
+    local activePage = theSpeckles.pages.index
 
-    if theSpeckles.pages.index == 1 then
+    if activePage == 1 then
         screen.move(7, 31)
         screen.text("Density")
         screen.stroke()
         screen.close()
-        oscUI.freq:redraw()
-    end
-
+        theSpeckles.oscUI.freq:redraw()
+    elseif activePage == 2 then
+        screen.move(5, 10)
+        screen.text("E2 - Freq: ")
+        screen.move(50, 10)
+        screen.text(string.format("%.2f", params:get('filter_freq')))
+        -- res
+        screen.move(5, 20)
+        screen.text("E3 - Res: ")
+        screen.move(50, 20)
+        screen.text(string.format("%.2f", params:get('reso')))
+        -- type
+        screen.move(70, 20)
+        screen.text("B3 - Type: ")
+        theSpeckles.filterUI.filterTypes:redraw()
+        if theSpeckles.filterUI.filter:get_active() then
+            theSpeckles.filterUI.filter:redraw()
+        end
     -- Render Instructions on last page
-    if theSpeckles.pages.index == 2 then
+    elseif activePage == 3 then
         -- found in ./lib/speckles-lib
         drawInstructions()
     end
